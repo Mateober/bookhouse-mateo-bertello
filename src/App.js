29aac3c10809f8cfd113a1f47c383ab8df038af3
Mateo <mateo.bertello@gmail.com>
2022-08-23 15:09:29 -0300

    Agregando Navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import "./App.css";
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <header>
+    <div className="nav container">
+      <i className="bx bx-menu burger-btn" id="burger-icon"></i>
+      <i className="bx bx-x cross-btn" id="cross-icon"></i>
+      <h1><a href="#" className="logo">BOOKHOUSE</a></h1>
+      <div className="headerSections">
+        <a href="#">Inicio</a>
+        <a href="#">Contacto</a>
+      </div>
     </div>
+  </header>
   );
-}
-
+};
 export default App;
